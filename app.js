18c4eb36e5ec97f90c373c32b7ce059dcf4f4e1e
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-01-11 13:13:40 -0700

    refactored the file structure

--- app.js
+++ app.js
@@ -1,100 +1,13 @@
-const fs = require("fs");
-const path = require("path");
-
 const express = require("express");
 
-// read tours from file
-let tours = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "dev-data", "data", "tours-simple.json"))
-);
+const tourRouter = require("./routes/tourRoutes");
 
 const app = express();
 
 app.use(express.json());
 
 // Tour Routes
-app.get("/api/tours", (req, res) => {
-  res.status(200).json({ status: "success", data: { tours } });
-});
-
-app.post("/api/tours", (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = { id: newId, ...req.body };
-  tours.push(newTour);
-
-  fs.writeFile(
-    path.join(__dirname, "dev-data", "data", "tours-simple.json"),
-    JSON.stringify(tours),
-    (err) => {
-      if (err) {
-        res.status(500).json({
-          status: "error",
-          message: "unable to write to tours file",
-          data: err,
-        });
-      }
-
-      res.status(201).json({ status: "success", data: { tour: newTour } });
-    }
-  );
-});
-
-app.get("/api/tours/:id", (req, res) => {
-  const id = +req.params.id;
-  if (id > tours.length) {
-    res
-      .status(400)
-      .json({ status: "fail", data: { id: `${id} is not a valid ID` } });
-  }
-
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({ status: "success", data: { tour } });
-});
-
-app.patch("/api/tours/:id", (req, res) => {
-  const id = +req.params.id;
-  if (id > tours.length) {
-    res
-      .status(400)
-      .json({ status: "fail", data: { id: `${id} is not a valid ID` } });
-  }
-
-  const tour = tours.find((el) => el.id === id);
-
-  for ([key, value] of Object.entries(req.body)) {
-    if (tour[key]) {
-      tour[key] = value;
-    }
-  }
-
-  res.status(200).json({ status: "success", data: { tour } });
-});
-
-app.delete("/api/tours/:id", (req, res) => {
-  const id = +req.params.id;
-  if (id > tours.length) {
-    res
-      .status(400)
-      .json({ status: "fail", data: { id: `${id} is not a valid ID` } });
-  }
-
-  tours = tours.filter((el) => el.id !== id);
-  fs.writeFile(
-    path.join(__dirname, "dev-data", "data", "tours-simple.json"),
-    JSON.stringify(tours),
-    (err) => {
-      if (err) {
-        res.status(500).json({
-          status: "error",
-          message: "unable to write to tours file",
-          data: err,
-        });
-      }
-      res.status(204).json({ status: "success", data: null });
-    }
-  );
-});
+app.use("/api/tours", tourRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
---
+++ controllers/toursController.js
@@ -0,0 +1,95 @@
+const fs = require("fs");
+const path = require("path");
+
+// read tours from file
+let tours = JSON.parse(
+  fs.readFileSync(
+    path.join(__dirname, "..", "dev-data", "data", "tours-simple.json")
+  )
+);
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({ status: "success", data: { tours } });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+
+  // write new tour to file
+  fs.writeFile(
+    path.join(__dirname, "..", "dev-data", "data", "tours-simple.json"),
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          status: "error",
+          message: "unable to write to tours file",
+          data: err,
+        });
+      }
+
+      res.status(201).json({ status: "success", data: { tour: newTour } });
+    }
+  );
+};
+
+exports.getTour = (req, res) => {
+  const id = +req.params.id;
+  if (id > tours.length) {
+    res
+      .status(400)
+      .json({ status: "fail", data: { id: `${id} is not a valid ID` } });
+  }
+
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({ status: "success", data: { tour } });
+};
+
+exports.deleteTour = (req, res) => {
+  const id = +req.params.id;
+  if (id > tours.length) {
+    res
+      .status(400)
+      .json({ status: "fail", data: { id: `${id} is not a valid ID` } });
+  }
+
+  tours = tours.filter((el) => el.id !== id);
+
+  // write updated tours to file
+  fs.writeFile(
+    path.join(__dirname, "..", "dev-data", "data", "tours-simple.json"),
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          status: "error",
+          message: "unable to write to tours file",
+          data: err,
+        });
+      }
+      res.status(204).json({ status: "success", data: null });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  const id = +req.params.id;
+  if (id > tours.length) {
+    res
+      .status(400)
+      .json({ status: "fail", data: { id: `${id} is not a valid ID` } });
+  }
+
+  const tour = tours.find((el) => el.id === id);
+
+  for ([key, value] of Object.entries(req.body)) {
+    if (tour[key]) {
+      tour[key] = value;
+    }
+  }
+
+  res.status(200).json({ status: "success", data: { tour } });
+};
