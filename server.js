fad02ba0d1c8732b371de5164a4509f3ba5f70b6
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-01-12 15:41:44 -0700

    added mongodb

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+// use config.env
 dotenv.config({ path: "./config.env" });
-
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "PASSWORD",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
