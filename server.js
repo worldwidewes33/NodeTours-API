aa584426f93d0fc3165964e771579628ba96d63c
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-01-13 17:35:50 -0700

    created tour model

--- server.js
+++ server.js
@@ -14,6 +14,27 @@ mongoose
   .then(() => console.log("Connected to database"))
   .catch((err) => console.log(err));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "a tour must have a name"],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    required: [true, "a tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const test = new Tour({ price: 350, name: "The Forest Hike" });
+test
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log("Error 💥", err));
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
