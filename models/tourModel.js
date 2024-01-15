7e86db8fa0e41e29f76acfa108cca620e75b26d5
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-01-15 09:40:54 -0700

    updated tour model to have more fields

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,10 +6,44 @@ const tourSchema = new mongoose.Schema({
     required: [true, "a tour must have a name"],
     unique: true,
   },
-  rating: { type: Number, default: 4.5 },
   price: {
     type: Number,
     required: [true, "a tour must have a price"],
+  },
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  summary: {
+    type: String,
+    required: [true, "A tour must have a summary"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  images: [String],
+  startDates: [Date],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
 });
 
