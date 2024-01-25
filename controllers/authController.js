61037d7d8c4ca5b2e8fa59e1c48db5a699ab0ead
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-01-25 09:44:27 -0700

    added jwt to signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,25 @@
 const User = require("./../models/userModel");
 const catchAsync = require("./../util/catchAsync");
+const jwt = require("jsonwebtoken");
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const { name, email, password, passwordConfirm } = req.body;
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+  });
+
+  console.log(process.env.JWT_EXPIRES_IN);
+
+  const token = await jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: "success",
+    token,
     user: newUser,
   });
 });
