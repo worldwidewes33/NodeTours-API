d05b63618b117c6f8e25a8830d8c3aa2eccf711c
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-01-27 18:11:18 -0700

    implemented password reset token

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,5 @@
+const crypto = require("crypto");
+
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
@@ -48,6 +50,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordUpdatedAt: Date,
+  passwordResetToken: String,
+  passwordResetTokenExpires: Date,
 });
 
 userSchema.pre("save", async function (next) {
@@ -79,6 +83,20 @@ userSchema.methods.passwordChangedAfter = function (timestamp) {
   return false;
 };
 
+userSchema.methods.generatePasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.passwordResetTokenExpires = Date.now() + 15 * 60 * 1000;
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  return resetToken;
+};
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
