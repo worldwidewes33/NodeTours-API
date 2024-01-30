100b272bd7fd2ad4eda2d18e3e67413769e89bd3
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-01-30 11:36:33 -0700

    refactored with eslint

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,35 +1,35 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const rootDir = require("./../../util/path");
-const Tour = require("./../../models/tourModel");
+const rootDir = require('../../util/path');
+const Tour = require('../../models/tourModel');
 
 // use config.env
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
-  "PASSWORD",
-  process.env.DATABASE_PASSWORD
+  'PASSWORD',
+  process.env.DATABASE_PASSWORD,
 );
 
 mongoose
   .connect(DB)
-  .then(() => console.log("Connected to database"))
+  .then(() => console.log('Connected to database'))
   .catch((err) => console.log(err));
 
 const tours = JSON.parse(
-  fs.readFileSync(path.join(rootDir, "tours-simple.json"))
+  fs.readFileSync(path.join(rootDir, 'tours-simple.json')),
 );
 
 const uploadTours = async () => {
   try {
     await Tour.create(tours);
-    console.log("Data successfully loaded");
+    console.log('Data successfully loaded');
   } catch (err) {
-    console.log("Error 💥", err);
+    console.log('Error 💥', err);
   }
   process.exit();
 };
@@ -37,15 +37,15 @@ const uploadTours = async () => {
 const deleteTours = async () => {
   try {
     await Tour.deleteMany();
-    console.log("Data successfully deleted");
+    console.log('Data successfully deleted');
   } catch (err) {
-    console.log("Error 💥", err);
+    console.log('Error 💥', err);
   }
   process.exit();
 };
 
-if (process.argv[2] === "--import") {
+if (process.argv[2] === '--import') {
   uploadTours();
-} else if (process.argv[2] === "--delete") {
+} else if (process.argv[2] === '--delete') {
   deleteTours();
 }
