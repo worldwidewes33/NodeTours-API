3944626989026a03f5251562191aab30a8398034
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-02-03 23:17:59 -0700

    finalized all route authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,20 +10,14 @@ router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch(
-  '/updatePassword',
-  authController.protect,
-  authController.updatePassword,
-);
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser,
-);
+router.use(authController.protect);
+
+router.patch('/updatePassword', authController.updatePassword);
+
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+router.get('/me', userController.getMe, userController.getUser);
 
 router.route('/').get(userController.getAllUsers);
 
